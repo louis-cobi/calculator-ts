f95b9d0038324b990bd1d6d801ace22e14ef05f4
louis-cobi <louis.cobigo@gmail.com>
2024-11-27 22:04:17 +0100

    rename Output into Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,9 @@
-interface OutputProps {
+interface InputProps {
     input: string;
     error: boolean;
   }
   
-  export default function Output({ input, error }: OutputProps) {
+  export default function Input({ input, error }: InputProps) {
     return (
       <div className="input-container">
         {error ? <h1>Error</h1> : <h1>{input ? input : "0"}</h1>}
